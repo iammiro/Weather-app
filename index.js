041c657b292d3b6bcdf9da5a7529927c4e895700
iammiro <mir.kolomiets@gmail.com>
2017-12-09 12:59:22 +0200

    edit data structures and wrap blocks in functions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const appSettings = {
     container: document.getElementById('container'),
-    apiUrl:'https://api.darksky.net/forecast/',
+    apiUrl: 'https://api.darksky.net/forecast/',
     proxy: 'https://cors-anywhere.herokuapp.com/',
     apiKey: 'c0edd7e111d453106e09ff75c17397b8',
     init: {
@@ -15,44 +15,43 @@ let longitude = -122.4233;
 
 const url = `${appSettings.proxy}${appSettings.apiUrl}${appSettings.apiKey}/${latitude},${longitude}`;
 
-function createNode(element) {
-    return document.createElement(element); // Create the type of element you pass in the parameters
-}
-
-function append(parent, el) {
-    return parent.appendChild(el); // Append the second parameter(element) to the first one
-}
-
-fetch(url, appSettings.init)
-    .then((response) => response.json())
-    .then(function (data) {
-        console.log(data);
-        let div = createNode('div');
-        div.innerHTML = `${data.currently.temperature}`;
-        append(appSettings.container, div);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-
-const getCurrentLocation = function () {
-    const options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-    };
-
-    function success(pos) {
-        let crd = pos.coords;
-        console.log(`Latitude : ${crd.latitude}`);
-        console.log(`Longitude: ${crd.longitude}`);
+const DOMManipulation = {
+    // Create the type of element you pass in the parameters
+    createNode: function (element) {
+        return document.createElement(element);
+    },
+    // Append the second parameter(element) to the first one
+    append: function (parent, el) {
+        return parent.appendChild(el);
     }
-
-    function error(err) {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
-
-    navigator.geolocation.getCurrentPosition(success, error, options);
 };
 
+const getApiData = () => {
+    fetch(url, appSettings.init)
+        .then((response) => response.json())
+        .then(function (data) {
+            console.log(data);
+            let div = DOMManipulation.createNode('div');
+            div.innerHTML = `${data.currently.temperature}`;
+            DOMManipulation.append(appSettings.container, div);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
+
+const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(pos => {
+            let crd = pos.coords;
+            console.log(`Latitude : ${crd.latitude}`);
+            console.log(`Longitude: ${crd.longitude}`);
+        }, err => console.warn(`ERROR(${err.code}): ${err.message}`),
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        });
+};
+
+getApiData();
 getCurrentLocation();
