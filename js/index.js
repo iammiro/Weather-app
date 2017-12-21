8034db39362fad7372b20e317bcebeeb860bc0fe
iammiro <mir.kolomiets@gmail.com>
2017-12-22 00:38:47 +0200

    edit css, make more usefull

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const appSettings = {
     apiUrl: 'https://api.darksky.net/forecast/',
     proxy: 'https://cors-anywhere.herokuapp.com/',
     apiKey: 'c0edd7e111d453106e09ff75c17397b8',
-    appURL: 'https://iammiro.github.io/Weather-app',
+    appURL: 'https://5c749cb1.ngrok.io',
     init: {
         method: 'GET',
         mode: 'cors',
@@ -267,7 +267,7 @@ getTodayForecast = () => {
             let pressure = document.getElementById('pressure');
             pressure.innerHTML = `Pressure: ${data.currently.pressure} hPa.`;
             let icon = document.getElementById('icon');
-            icon.src = `/img/${data.currently.icon}.svg`;
+            icon.src = `img/${data.currently.icon}.svg`;
             let summary = document.getElementById('summary');
             summary.innerHTML = `Today: ${data.currently.temperature} ${units.get('temperature')}. ${data.currently.summary}`;
             let hourlySummary = document.getElementById('hourlySummary');
@@ -301,7 +301,7 @@ getWeekForecast = () => {
                 let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
                 let day = days[dayNumber.getDay()];
                 let icon = document.getElementById(`icon-${k - 1}`);
-                icon.src = `/img/${element.icon}.svg`;
+                icon.src = `img/${element.icon}.svg`;
                 let header = document.getElementById(`header-${k - 1}`);
                 header.innerHTML = `${day}`;
                 let summary = document.getElementById(`summary-${k - 1}`);
